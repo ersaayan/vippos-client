cbd07428eff89432b86e6f391f70c81c671178f0
Eren ŞAVK <ersaayan@gmail.com>
2024-04-30 15:38:40 +0300

    Update HttpClient provider in app.config.ts and add a wildcard route in app.routes.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,13 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { MessageService } from 'primeng/api';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient( withFetch()),
     provideClientHydration(),
     provideAnimationsAsync(),
     MessageService,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,5 +17,9 @@ export const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
   }
 ];
