6f11be34a994a3768d2db698826a417f1f8decc5
Eren ŞAVK <ersaayan@gmail.com>
2024-05-17 16:13:58 +0300

    guard fixed

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,14 +1,22 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  if (
-    typeof localStorage !== 'undefined' &&
-    localStorage.getItem('accessToken')
-  ) {
-    return true;
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (localStorage.getItem('accessToken')) {
+    if (authService.isLoggedIn()) {
+      return true;
+    } else {
+      authService.logout();
+      router.navigate(['login']);
+      return false; // Navigasyon sonrasında false dönmek daha güvenli
+    }
   } else {
-    const router = inject(Router);
-    return router.navigate(['login']);
+    authService.logout();
+    router.navigate(['login']);
+    return false; // Navigasyon sonrasında false dönmek daha güvenli
   }
 };
